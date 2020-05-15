9803eef9df922bf23d2e187d04aee6b2196d4ed3
Phuc <sucanabo@gmail.com>
2020-05-15 12:16:27 +0700

    kiem tra code branch catlauoutfashi

--- php/content/blog-sidebar.php
+++ php/content/blog-sidebar.php
@@ -1,10 +1,10 @@
 <div class="col-lg-3 col-md-6 col-sm-8 order-2 order-lg-1">
     <div class="blog-sidebar">
     <?php 
-        include ($level.php_path.content_path.'search-form.php')
-        include ($level.php_path.content_path.'blog-catagory.php')
-        include ($level.php_path.content_path.'recent-post.php')
-        include ($level.php_path.content_path.'blog-tags.php')
+        include ($level.php_path.content_path.'search-form.php');
+        include ($level.php_path.content_path.'blog-catagory.php');
+        include ($level.php_path.content_path.'recent-post.php');
+        include ($level.php_path.content_path.'blog-tags.php');
     ?>
     </div>
 </div>
