0e5c3be4033b0ec897d407e82a9a03632866f830
okok811 <nxtruong0811@gmail.com>
2020-06-18 17:07:58 +0700

    fix view

--- php/content/index-women-banner.php
+++ php/content/index-women-banner.php
@@ -36,7 +36,7 @@
                                 </div>
                                 <ul>
                                     <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                                    <li class="quick-view"><a href="#"><?php echo $a['view']?></a></li>
+                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
                                     <li class="w-icon"><a href="#"><i class="fa fa-random"></i></a></li>
                                 </ul>
                             </div>
