ed375da237f40d74af1f8d121fdb18df9a52b667
unknown <buiduongctld@gmail.com>
2020-06-25 22:35:29 +0700

    addtocart thieu so luong

--- data/shoppingcart_data.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $cart = DP::run_query ("SELECT * FROM `cart`",[],2);
-    var_dump($cart);
-?>
--- php/content/cart-table.php
+++ php/content/cart-table.php
@@ -1,26 +1,4 @@
 <?php
-session_start();
-var_dump($_POST);
-
-if(isset($_POST['submit']))
-{
-
-	foreach($_POST as $prid=>$soluong)
-	{
-		if( ($soluong == 0) and (is_numeric($soluong)))
-		{
-			unset ($_SESSION['cart'][$prid]);
-		}
-		elseif(($soluong > 0) and (is_numeric($soluong)))
-		{
-			$_SESSION['cart'][$prid]['soluong']=$soluong;
-		}
-		
-	}
-	//header("location:cart.php");
-}
-?>
-<?php
 $checkcart=false;
 $total=0;
 if(isset($_SESSION['cart']))
@@ -37,7 +15,7 @@ if($checkcart== true)
 {	
     ?>
 <div class="col-lg-12">
-<form action="<?php echo $level.data_path.'add_cart.php'?>">
+<form action="<?php echo $level.pages_path.'shopping-cart.php'?>">
     <div class="cart-table">
         <table>
             <thead>
@@ -53,25 +31,27 @@ if($checkcart== true)
             <tbody>
                 
                 <?php 
-                    foreach ($cart as $key => $p) {
+                    foreach ($_SESSION['cart'] as $cartitem) {
                         ?>
                         <tr>
-                            <td class="cart-pic first-row"><img src="<?php echo $level.img_path.$p['img']?>" alt=""></td>
+                            <td class="cart-pic first-row"><img src="<?php echo $level.img_path.$cartitem['img']?>" alt=""></td>
                             <td class="cart-title first-row">
-                                <h5><?php echo $p['name']?></h5>
+                                <h5><?php echo $cartitem['name']?></h5>
                             </td>
-                            <td class="p-price first-row"><?php echo '$'.$p['pprice']?></td>
+                            <td class="p-price first-row"><?php echo '$'.$cartitem['price']?></td>
                             <td class="qua-col first-row">
                                 <div class="quantity">
                                     <div class="pro-qty">
-                                        <input type="text" value="1">
+                                        <input type="text" value="<?php echo $cartitem['quantity']?>">
                                     </div>
                                 </div>
                             </td>
-                            <td class="total-price first-row"><?php echo '$'.$p['tprice']?></td>
-                            <td class="close-td first-row"><i class="ti-close"></i></td>
+                            <td class="total-price first-row"><?php echo '$'.$cartitem['price']*$cartitem['quantity']?></td>
+                            <td class="close-td first-row"><a href="<?php echo $level.data_path."delcart.php?id=".$cartitem['productid']?>"><i class="ti-close"></i></a></td>
                         </tr>
+                        
                         <?php
+                        $total+=$cartitem['quantity']*$cartitem['price'];
                     }
                 ?>
                 
@@ -79,11 +59,38 @@ if($checkcart== true)
             </tbody>
         </table>
     </div>
+    
     <?php		
 	}
 
 else
 	{
 		echo "Ban chua mua sp nao.";
-	}
+    }
+    
 ?>
+<div class="row">
+    <div class="col-lg-4">
+        <div class="cart-buttons">
+            <a href="<?php echo $level.pages_path.'shop.php'?>" class="primary-btn continue-shop">Continue shopping</a>
+            <input type = "submit" value = "Update cart" class="primary-btn up-cart"></input>
+        </div>
+        <div class="discount-coupon">
+            <h6>Discount Codes</h6>
+            <form action="<?php echo $level.pages_path.'check-out.php'?>" class="coupon-form">
+                <input type="text" placeholder="Enter your codes">
+                <button type="" class="site-btn coupon-btn">Apply</button>
+            </form>
+        </div>
+    </div>
+    <div class="col-lg-4 offset-lg-4">
+        <div class="proceed-checkout">
+            <ul>
+                <!-- <li class="subtotal">Subtotal <span>$240.00</span></li> -->
+                <li class="cart-total">Total <span><?php echo "$".$total ?></span></li>
+            </ul>
+            <a href="<?php echo $level.pages_path.'check-out.php'?>" class="proceed-btn">PROCEED TO CHECK OUT</a>
+        </div>
+    </div>
+</div>
+</div>
